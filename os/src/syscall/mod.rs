57d9342363a51383ae41cef25fcd25267cebd1f6
niceskylei <niceskylei@gmail.com>
2024-05-15 00:57:06 +0000

    fix task info

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -27,14 +27,18 @@ pub use process::TaskInfo;
 
 use fs::*;
 use process::*;
+
+use crate::task::inc_current_task_syscall;
 /// handle syscall exception with `syscall_id` and other arguments
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
-    match syscall_id {
+    inc_current_task_syscall(syscall_id);
+    let ret = match syscall_id {
         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
         SYSCALL_EXIT => sys_exit(args[0] as i32),
         SYSCALL_YIELD => sys_yield(),
         SYSCALL_GET_TIME => sys_get_time(args[0] as *mut TimeVal, args[1]),
         SYSCALL_TASK_INFO => sys_task_info(args[0] as *mut TaskInfo),
         _ => panic!("Unsupported syscall_id: {}", syscall_id),
-    }
+    };
+    ret
 }
